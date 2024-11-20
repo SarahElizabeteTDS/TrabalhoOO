6f8718e2226b0b6755f74c55277fe06f4958860e
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2024-11-20 16:26:30 -0300

    Delete trabalhoOO directory

--- trabalhoOO/modelo/IPessoa.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-interface IPessoa
-{
-    public function listarTodosLivros($livros);
-    public function listarLivrosDisponiveis($livros);
-    public function listarLivrosEmprestados();
-}
