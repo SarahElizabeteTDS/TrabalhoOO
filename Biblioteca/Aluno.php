f475f1adfff9b2d5048db65815e6bf843d5f4f5a
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2024-11-19 23:51:36 -0300

    Delete Biblioteca directory

--- Biblioteca/Aluno.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'Pessoa.php';
-
-class Aluno extends Pessoa {
-    public function __construct($nome){
-        parent::__construct($nome);
-    }
-}
--- Biblioteca/lPessoa.php
+++
@@ -1,6 +0,0 @@
-<?php
-interface lPessoa{
-    public function emprestarLivro(Livro $livro);
-    public function devolverLivro(Livro $livro);
-    public function listarLivrosEmprestados();
-}
